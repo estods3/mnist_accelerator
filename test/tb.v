09233ed490e88929ae84914a9df8537c5e309410
Evan <estods76@gmail.com>
2024-07-11 16:16:56 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -24,7 +24,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_and_gate_example user_project (
+  tt_um_and_gate_example user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
