76795a6119d8c524fa0f21a8c5a76fe6545053ce
Evan <estods76@gmail.com>
2024-07-16 20:59:11 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,8 +16,17 @@ module tt_um_estods3_nnaccelerator (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  wire [6:0] led_out;
+  assign uo_out[6:0] = led_out;
+  assign uo_out[7] = 1'b0;
+
+  reg [3:0] digit;
+  digit <= 0;
+
+  // instantiate segment display
+  seg7 seg7(.counter(digit), .segments(led_out));
+    
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in & uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
